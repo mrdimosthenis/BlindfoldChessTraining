d4e3c3af4dc7391cff6327f361e0dd9c846dabbe
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-09-27 01:03:30 +0300

    should fix controlled by functions

--- BlindfoldChessMechanics/Model.Board.fs
+++ BlindfoldChessMechanics/Model.Board.fs
@@ -1,5 +1,8 @@
 ﻿module BlindfoldChessMechanics.Model.Board
 
+exception InvalidMove
+exception NoPieceInPosition
+
 // types
 
 type Move = int * int * int * int
@@ -32,10 +35,15 @@ let blackPawn: Resident = Some {PieceType=Pawn; IsWhite=false}
 
 // utils
 
-let toArrays (board: Board) =
-        board
-        |> Seq.map Seq.toArray
-        |> Seq.toArray
+let toArrays (board: Board): Resident array array =
+    board
+    |> Seq.map Seq.toArray
+    |> Seq.toArray
+
+let ofArrays (arr: Resident array array): Board =
+    arr
+    |> Seq.ofArray
+    |> Seq.map Seq.ofArray
 
 let updatedSeq<'a> (index: int) (newItem: 'a) (s: 'a seq): 'a seq =
     s
@@ -47,31 +55,108 @@ let updatedSeq<'a> (index: int) (newItem: 'a) (s: 'a seq): 'a seq =
 // actual implementation
 
 let init: Board =
-        [|[|blackRook; blackKnight; blackBishop; blackQueen; blackKing; blackBishop; blackKnight; blackRook|]
-          [|blackPawn; blackPawn; blackPawn; blackPawn; blackPawn; blackPawn; blackPawn; blackPawn|]
-          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-          [|whitePawn; whitePawn; whitePawn; whitePawn; whitePawn; whitePawn; whitePawn; whitePawn|]
-          [|whiteRook; whiteKnight; whiteBishop; whiteQueen; whiteKing; whiteBishop; whiteKnight; whiteRook|]|]
-        |> Seq.ofArray
-        |> Seq.map Seq.ofArray
-        |> Seq.rev
+    [|[|blackRook; blackKnight; blackBishop; blackQueen; blackKing; blackBishop; blackKnight; blackRook|]
+      [|blackPawn; blackPawn; blackPawn; blackPawn; blackPawn; blackPawn; blackPawn; blackPawn|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|whitePawn; whitePawn; whitePawn; whitePawn; whitePawn; whitePawn; whitePawn; whitePawn|]
+      [|whiteRook; whiteKnight; whiteBishop; whiteQueen; whiteKing; whiteBishop; whiteKnight; whiteRook|]|]
+    |> Seq.ofArray
+    |> Seq.map Seq.ofArray
+    |> Seq.rev
+        
+let afterMove (move: Move) (board: Board): Board =
+    let (fromRowIndex, fromColumnIndex, toRowIndex, toColumnIndex) = move
+    let fromRow = Seq.item fromRowIndex board
+    let fromResident = Seq.item fromColumnIndex fromRow
+    let toRow = Seq.item toRowIndex board
+    let toResident = Seq.item toColumnIndex toRow
+    match (fromResident, toResident) with
+    | (None, _) | (Some {IsWhite=false}, Some {IsWhite=false}) |(Some {IsWhite=true}, Some {IsWhite=true}) -> raise InvalidMove
+    | _ -> let updatedFromRow = updatedSeq fromColumnIndex None fromRow
+           let updatedToRow = updatedSeq toColumnIndex fromResident toRow
+           board
+           |> updatedSeq fromRowIndex updatedFromRow
+           |> updatedSeq toRowIndex updatedToRow
 
-let afterMove (move: Move) (board: Board): Board Option =
-        let (fromRowIndex, fromColumnIndex, toRowIndex, toColumnIndex) = move
-        let fromRow = Seq.item fromRowIndex board
-        let fromResident = Seq.item fromColumnIndex fromRow
-        let toRow = Seq.item toRowIndex board
-        let toResident = Seq.item toColumnIndex toRow
-        match (fromResident, toResident) with
-        | (None, _) | (Some {IsWhite=false}, Some {IsWhite=false}) |(Some {IsWhite=true}, Some {IsWhite=true}) -> None
-        | _ -> let updatedFromRow = updatedSeq fromColumnIndex None fromRow
-               let updatedToRow = updatedSeq toColumnIndex fromResident toRow
-               board
-               |> updatedSeq fromRowIndex updatedFromRow
-               |> updatedSeq toRowIndex updatedToRow
-               |> Some
+let upIndices (position: int * int): (int * int) seq =
+    let (rowIndex, columnIndex) = position
+    Seq.init (7 - rowIndex) (fun i -> (rowIndex + i + 1, columnIndex))
+
+let downIndices (position: int * int): (int * int) seq =
+    let (rowIndex, columnIndex) = position
+    Seq.init rowIndex (fun i -> (rowIndex - i - 1, columnIndex))
+
+let leftIndices (position: int * int): (int * int) seq =
+    let (rowIndex, columnIndex) = position
+    Seq.init columnIndex (fun i -> (rowIndex, columnIndex - i - 1))
+
+let rightIndices (position: int * int): (int * int) seq =
+    let (rowIndex, columnIndex) = position
+    Seq.init (7 - columnIndex) (fun i -> (rowIndex, columnIndex + i + 1))
+
+let upRightDiagonal (position: int * int): (int * int) seq =
+    let (rowIndex, columnIndex) = position
+    let minDist = min (7 - rowIndex) (7 - columnIndex)
+    Seq.init minDist (fun i -> (rowIndex + i + 1, columnIndex + i + 1))
+
+let downRightDiagonal (position: int * int): (int * int) seq =
+    let (rowIndex, columnIndex) = position
+    let minDist = min rowIndex (7 - columnIndex)
+    Seq.init minDist (fun i -> (rowIndex - i - 1, columnIndex + i + 1))
+
+let upLeftDiagonal (position: int * int): (int * int) seq =
+    let (rowIndex, columnIndex) = position
+    let minDist = min (7 - rowIndex) columnIndex
+    Seq.init minDist (fun i -> (rowIndex + i + 1, columnIndex - i - 1))
+
+let downLeftDiagonal (position: int * int): (int * int) seq =
+    let (rowIndex, columnIndex) = position
+    let minDist = min rowIndex columnIndex
+    Seq.init minDist (fun i -> (rowIndex - i - 1, columnIndex - i - 1))
+
+let resident (position: int * int) (board: Board): Resident =
+    let (rowIndex, columnIndex) = position
+    board
+    |> Seq.item rowIndex
+    |> Seq.item columnIndex
+
+let indicesControlledByRook (position: int * int) (board: Board): (int * int) seq =
+    match resident position board with
+    | None ->
+        raise NoPieceInPosition
+    | Some {IsWhite=isWhite}
+        -> let takeOrNot pos = match resident pos board with
+                               | Some {IsWhite=w} when w = isWhite -> false
+                               | _ -> true
+           [|position |> upIndices |> Seq.takeWhile takeOrNot
+             position |> downIndices |> Seq.takeWhile takeOrNot
+             position |> rightIndices |> Seq.takeWhile takeOrNot
+             position |> leftIndices |> Seq.takeWhile takeOrNot|]
+           |> Seq.ofArray
+           |> Seq.concat
+
+let indicesControlledByBishop (position: int * int) (board: Board): (int * int) seq =
+    match resident position board with
+    | None ->
+        raise NoPieceInPosition
+    | Some {IsWhite=isWhite}
+        -> let takeOrNot pos = match resident pos board with
+                               | Some {IsWhite=w} when w = isWhite -> false
+                               | _ -> true
+           [|position |> upRightDiagonal |> Seq.takeWhile takeOrNot
+             position |> downRightDiagonal |> Seq.takeWhile takeOrNot
+             position |> upLeftDiagonal |> Seq.takeWhile takeOrNot
+             position |> downLeftDiagonal |> Seq.takeWhile takeOrNot|]
+           |> Seq.ofArray
+           |> Seq.concat
+
+let indicesControlledByQueen (position: int * int) (board: Board): (int * int) seq =
+    [|indicesControlledByRook position board
+      indicesControlledByBishop position board|]
+    |> Seq.ofArray
+    |> Seq.concat
--- BlindfoldChessTest/BlindfoldChessMechanics.Model.BoardTest.fs
+++ BlindfoldChessTest/BlindfoldChessMechanics.Model.BoardTest.fs
@@ -9,7 +9,6 @@ open BlindfoldChessMechanics.Model.Board
 let ``Board after valid pawn move`` () =
     Board.init
     |> afterMove (1, 0, 3, 0)
-    |> Option.get
     |> Seq.rev
     |> toArrays
     |> should equal
@@ -28,7 +27,6 @@ let ``Board after valid pawn move`` () =
 let ``Board after valid knight move`` () =
     Board.init
     |> afterMove (0, 6, 2, 5)
-    |> Option.get
     |> Seq.rev
     |> toArrays
     |> should equal
@@ -45,12 +43,26 @@ let ``Board after valid knight move`` () =
 
 [<Fact>]
 let ``Board after move without piece`` () =
-    Board.init
-    |> afterMove (3, 3, 4, 3)
-    |> should equal None
+    (fun () -> afterMove (3, 3, 4, 3) Board.init |> ignore)
+    |> should throw typeof<InvalidMove>
 
 [<Fact>]
 let ``Board after move which captures same color`` () =
-    Board.init
-    |> afterMove (0, 7, 1, 7)
-    |> should equal None
+    (fun () -> afterMove (0, 7, 1, 7) Board.init |> ignore)
+    |> should throw typeof<InvalidMove>
+
+[<Fact>]
+let ``Indices cotrolled by rook`` () =
+    [|[|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; whiteRook; emptySquare; emptySquare; emptySquare; whiteBishop; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; whiteKnight; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; blackBishop; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+      [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]|]
+    |> ofArrays
+    |> indicesControlledByRook (5, 2)
+    |> should equal Seq.empty
