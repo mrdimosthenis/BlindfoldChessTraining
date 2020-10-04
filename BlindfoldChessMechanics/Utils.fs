f7cd11df82942d58440e6a8e6587c511ad15ee0f
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-10-04 23:58:23 +0300

    updatedSequences

--- BlindfoldChessMechanics/Utils.fs
+++ BlindfoldChessMechanics/Utils.fs
@@ -12,4 +12,9 @@ let updatedSeq<'a> (index: int) (newItem: 'a) (s: 'a seq): 'a seq =
     |> Seq.indexed
     |> Seq.map (fun (i, item) -> if i = index then newItem else item)
 
+let updatedSequences<'a> (indices: int * int) (newItem: 'a) (s: 'a seq seq): 'a seq seq =
+    let (i, j) = indices
+    let updatedInnerSeq = Seq.item i s |> updatedSeq j newItem
+    updatedSeq i updatedInnerSeq s
+
 let prependedSeq<'a> (a: 'a) (s: 'a seq): 'a seq = Seq.append (seq [ a ]) s
