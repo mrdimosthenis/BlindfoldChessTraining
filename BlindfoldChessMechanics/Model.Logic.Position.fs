37f85a64a0823a4d4e4aacb82267931ade5f82c3
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-10-07 12:21:38 +0300

    special King Moves

--- BlindfoldChessMechanics/Model.Logic.Position.fs
+++ BlindfoldChessMechanics/Model.Logic.Position.fs
@@ -32,37 +32,30 @@ type Position = { CurrentBoard: Board.Board
                   Halfmove: int
                   Fullmove: int }
 
-type CastleMove =
-    | WhiteKingSideCastle 
-    | WhiteQueenSideCastle
-    | BlackKingSideCastle
-    | BlackQueenSideCastle
-
 let specialKingMoves (coordinates: Board.Coordinates) (position: Position): Move seq =
     let (rowIndex, columnIndex) = coordinates
     match Board.resident coordinates position.CurrentBoard with
     | Some { PieceType = Board.King; IsWhite = isWhite } ->
         let controlledCoordsByOpponent = Board.coordinatesControlledByColor (not isWhite) position.CurrentBoard
-        [| (WhiteKingSideCastle, position.Castling.WhiteKingSideCastle, (+))
-           (WhiteQueenSideCastle, position.Castling.WhiteQueenSideCastle, (-))
-           (BlackKingSideCastle, position.Castling.BlackKingSideCastle, (+))
-           (BlackQueenSideCastle, position.Castling.BlackQueenSideCastle, (-)) |]
+        [| (true, position.Castling.WhiteKingSideCastle, (+))
+           (true, position.Castling.WhiteQueenSideCastle, (-))
+           (false, position.Castling.BlackKingSideCastle, (+))
+           (false, position.Castling.BlackQueenSideCastle, (-)) |]
         |> Seq.ofArray
-        |> Seq.map ( fun (castleMove, ability, op) ->
-                        (
-                            castleMove,
-                            ability && (Seq.forall ((<>) coordinates) controlledCoordsByOpponent)
-                                    && (Seq.forall ((<>) (op rowIndex 1, columnIndex)) controlledCoordsByOpponent)
-                                    && (Seq.forall ((<>) (op rowIndex 2, columnIndex)) controlledCoordsByOpponent)
-                        )
+        |> Seq.map ( fun (isWh, ability, op) ->
+                        ability && isWh = isWhite
+                        && Board.resident (rowIndex, op columnIndex 1) position.CurrentBoard = None
+                        && Board.resident (rowIndex, op columnIndex 2) position.CurrentBoard = None
+                        && (Seq.forall ((<>) coordinates) controlledCoordsByOpponent)
+                        && (Seq.forall ((<>) (rowIndex, op columnIndex 1)) controlledCoordsByOpponent)
+                        && (Seq.forall ((<>) (rowIndex, op columnIndex 2)) controlledCoordsByOpponent)
                    )
+        |> Seq.zip (Seq.ofArray [| (rowIndex, columnIndex + 2)
+                                   (rowIndex, columnIndex - 2)
+                                   (rowIndex, columnIndex + 2)
+                                   (rowIndex, columnIndex - 2) |])
         |> Seq.filter snd
-        |> Seq.map (fun (castleMove, _) ->
-                        match castleMove with
-                        | WhiteKingSideCastle | BlackKingSideCastle -> (rowIndex + 2, columnIndex)
-                        | WhiteQueenSideCastle | BlackQueenSideCastle -> (rowIndex - 2, columnIndex)
-                   )
-        |> Seq.map (fun toCoords ->
+        |> Seq.map (fun (toCoords, _) ->
                         { Piece = Board.King
                           FromCoords = coordinates
                           ToCoords = toCoords
--- BlindfoldChessTest/BlindfoldChessMechanics.Model.Logic.PositionTest.fs
+++ BlindfoldChessTest/BlindfoldChessMechanics.Model.Logic.PositionTest.fs
@@ -4,45 +4,132 @@ open Xunit
 open FsUnit.Xunit
 
 open BlindfoldChessMechanics.Model.Logic.Position
+open BlindfoldChessMechanics.Model.Logic.Board
 
-//[<Fact>]
-//let ``Board after valid pawn move`` () =
-//    Board.init
-//    |> afterMove (1, 0, 3, 0)
-//    |> Seq.rev
-//    |> toArrays
-//    |> should equal
-//        [|[|blackRook;   blackKnight; blackBishop; blackQueen;  blackKing;   blackBishop; blackKnight; blackRook|]
-//          [|blackPawn;   blackPawn;   blackPawn;   blackPawn;   blackPawn;   blackPawn;   blackPawn;   blackPawn|]
-//          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-//          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-//          [|whitePawn;   emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-//          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-//          [|emptySquare; whitePawn;   whitePawn;   whitePawn;   whitePawn;   whitePawn;   whitePawn;   whitePawn|]
-//          [|whiteRook;   whiteKnight; whiteBishop; whiteQueen;  whiteKing;   whiteBishop; whiteKnight; whiteRook|]|]
-//
-//[<Fact>]
-//let ``Board after valid knight move`` () =
-//    Board.init
-//    |> afterMove (0, 6, 2, 5)
-//    |> Seq.rev
-//    |> toArrays
-//    |> should equal
-//        [|[|blackRook;   blackKnight; blackBishop; blackQueen;  blackKing;   blackBishop; blackKnight; blackRook|]
-//          [|blackPawn;   blackPawn;   blackPawn;   blackPawn;   blackPawn;   blackPawn;   blackPawn;   blackPawn|]
-//          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-//          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-//          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
-//          [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; whiteKnight; emptySquare; emptySquare|]
-//          [|whitePawn;   whitePawn;   whitePawn;   whitePawn;   whitePawn;   whitePawn;   whitePawn;   whitePawn|]
-//          [|whiteRook;   whiteKnight; whiteBishop; whiteQueen;  whiteKing;   whiteBishop; emptySquare; whiteRook|]|]
-//
-//[<Fact>]
-//let ``Board after move without piece`` () =
-//    (fun () -> afterMove (3, 3, 4, 3) Board.init |> ignore)
-//    |> should throw typeof<InvalidMove>
-//
-//[<Fact>]
-//let ``Board after move which captures same color`` () =
-//    (fun () -> afterMove (0, 7, 1, 7) Board.init |> ignore)
-//    |> should throw typeof<InvalidMove>
+open BlindfoldChessMechanics
+
+[<Fact>]
+let ``Special white king moves with castling ability`` () =
+    { CurrentBoard = [|[|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|whiteRook;   emptySquare; emptySquare; emptySquare; whiteKing;   emptySquare; emptySquare; whiteRook|]|]
+                     |> Utils.seqOfArrays
+                     |> Seq.rev
+      IsWhiteToMove = true
+      Castling = { WhiteKingSideCastle = true
+                   WhiteQueenSideCastle = true
+                   BlackKingSideCastle = true
+                   BlackQueenSideCastle = true }
+      EnPassant = None
+      Halfmove = 12
+      Fullmove = 20 }
+    |> specialKingMoves (0, 4)
+
+    |> Seq.toArray
+    |> should equal
+        [| { Piece = King
+             FromCoords = (0, 4)
+             ToCoords = (0, 6)
+             IsCapture = false
+             IsCheck = false
+             Promotion = None }
+           { Piece = King
+             FromCoords = (0, 4)
+             ToCoords = (0, 2)
+             IsCapture = false
+             IsCheck = false
+             Promotion = None } |]
+
+[<Fact>]
+let ``Special white king moves without castling ability`` () =
+    { CurrentBoard = [|[|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|whiteRook;   emptySquare; emptySquare; emptySquare; whiteKing;   emptySquare; emptySquare; whiteRook|]|]
+                     |> Utils.seqOfArrays
+                     |> Seq.rev
+      IsWhiteToMove = true
+      Castling = { WhiteKingSideCastle = false
+                   WhiteQueenSideCastle = false
+                   BlackKingSideCastle = true
+                   BlackQueenSideCastle = true }
+      EnPassant = None
+      Halfmove = 12
+      Fullmove = 20 }
+    |> specialKingMoves (0, 4)
+
+    |> Seq.toArray
+    |> should equal
+        [||]
+
+[<Fact>]
+let ``Special black king moves with castling ability`` () =
+    { CurrentBoard = [|[|blackRook;   emptySquare; emptySquare; emptySquare; blackKing;   emptySquare; emptySquare; blackRook|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|whiteRook;   emptySquare; emptySquare; emptySquare; whiteKing;   emptySquare; emptySquare; whiteRook|]|]
+                     |> Utils.seqOfArrays
+                     |> Seq.rev
+      IsWhiteToMove = true
+      Castling = { WhiteKingSideCastle = true
+                   WhiteQueenSideCastle = true
+                   BlackKingSideCastle = true
+                   BlackQueenSideCastle = true }
+      EnPassant = None
+      Halfmove = 12
+      Fullmove = 20 }
+    |> specialKingMoves (7, 4)
+
+    |> Seq.toArray
+    |> should equal
+        [| { Piece = King
+             FromCoords = (7, 4)
+             ToCoords = (7, 6)
+             IsCapture = false
+             IsCheck = false
+             Promotion = None }
+           { Piece = King
+             FromCoords = (7, 4)
+             ToCoords = (7, 2)
+             IsCapture = false
+             IsCheck = false
+             Promotion = None } |]
+
+[<Fact>]
+let ``Special black king moves without castling ability`` () =
+    { CurrentBoard = [|[|blackRook;   emptySquare; emptySquare; emptySquare; blackKing;   blackBishop; emptySquare; blackRook|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare; emptySquare|]
+                       [|emptySquare; emptySquare; whiteRook;   emptySquare; whiteKing;   emptySquare; emptySquare; whiteRook|]|]
+                     |> Utils.seqOfArrays
+                     |> Seq.rev
+      IsWhiteToMove = true
+      Castling = { WhiteKingSideCastle = true
+                   WhiteQueenSideCastle = true
+                   BlackKingSideCastle = true
+                   BlackQueenSideCastle = true }
+      EnPassant = None
+      Halfmove = 12
+      Fullmove = 20 }
+    |> specialKingMoves (7, 4)
+
+    |> Seq.toArray
+    |> should equal
+        [||]
