6f5ecee0c1ab5f5edaea633c2faaef4d6e186c95
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-10-11 02:07:41 +0300

    movement name with tests

---
+++ BlindfoldChessMechanics/Notation.Emit.fs
@@ -0,0 +1,80 @@
+﻿module BlindfoldChessMechanics.Notation.Emit
+
+open BlindfoldChessMechanics.Logic
+
+exception InvalidColumn
+
+// functions
+
+let rowName(r: int): string =
+    string (r + 1)
+
+let columnName(c: int): string =
+    match c with
+    | 0 -> "a"
+    | 1 -> "b"
+    | 2 -> "c"
+    | 3 -> "d"
+    | 4 -> "e"
+    | 5 -> "f"
+    | 6 -> "g"
+    | 7 -> "h"
+    | _ -> raise InvalidColumn
+
+let pieceName (isWhite: bool) (areFigures: bool) (p: Board.Piece): string =
+    match (p, areFigures, isWhite) with
+    | (Board.King, false, _) -> "K"
+    | (Board.Queen, false, _) -> "Q"
+    | (Board.Rook, false, _) -> "R"
+    | (Board.Bishop, false, _) -> "B"
+    | (Board.Knight, false, _) -> "N"
+    | (Board.Pawn, false, _) -> "P"
+    | (Board.King, true, true) -> "♔"
+    | (Board.Queen, true, true) -> "♕"
+    | (Board.Rook, true, true) -> "♖"
+    | (Board.Bishop, true, true) -> "♗"
+    | (Board.Knight, true, true) -> "♘"
+    | (Board.Pawn, true, true) -> "♙"
+    | (Board.King, true, false) -> "♚"
+    | (Board.Queen, true, false) -> "♛"
+    | (Board.Rook, true, false) -> "♜"
+    | (Board.Bishop, true, false) -> "♝"
+    | (Board.Knight, true, false) -> "♞"
+    | (Board.Pawn, true, false) -> "♟︎"
+
+let movementName (isWhite: bool) (areFigures: bool) (m: Position.Movement): string =
+    match (m.Piece, snd m.FromCoords, snd m.ToCoords) with
+    | (Board.King, 4, 6) -> "0-0"
+    | (Board.King, 4, 2) -> "0-0-0"
+    | _ -> let piece = match m.Piece with
+                       | Board.Pawn -> ""
+                       | _ -> pieceName isWhite areFigures m.Piece
+           let clarification =
+               match (m.Piece, m.SamePieceCoords) with
+               | (Board.Pawn, _) -> m.FromCoords
+                                    |> snd
+                                    |> columnName
+               | (_, Some (_, c)) when c = (snd m.FromCoords) -> m.FromCoords
+                                                                 |> fst
+                                                                 |> rowName
+               | (_, Some _) -> m.FromCoords
+                                |> snd
+                                |> columnName
+               | _ -> ""
+           let takes = if m.IsCapture then "x" else ""
+           let targetColumn = m.ToCoords
+                              |> snd
+                              |> columnName
+           let targetSquare = m.ToCoords
+                              |> fst
+                              |> rowName
+           let promotion = match m.Promotion with
+                           | Some p -> "=" + pieceName isWhite areFigures p
+                           | None -> ""
+           let checkOrMate = match (m.IsCheck, m.IsMate) with
+                             | (_, true) -> "#"
+                             | (true, _) -> "+"
+                             | _ -> ""
+           piece + clarification + takes + targetColumn + targetSquare + promotion + checkOrMate
+
+
---
+++ BlindfoldChessTest/BlindfoldChessMechanics.Notation.EmitTest.fs
@@ -0,0 +1,99 @@
+﻿module BlindfoldChessMechanics.Notation.EmitTest
+
+open Xunit
+open FsUnit.Xunit
+
+open BlindfoldChessMechanics.Notation.Emit
+open BlindfoldChessMechanics.Logic.Board
+open BlindfoldChessMechanics.Logic.Position
+
+[<Fact>]
+let ``0-0 movement name`` () =
+    { Piece = King
+      FromCoords = (0, 4)
+      ToCoords = (0, 6)
+      IsCapture = false
+      Promotion = None
+      IsCheck = false
+      IsMate = false
+      IsStalemate = false
+      SamePieceCoords = None }    
+    |> movementName true false
+    |> should equal
+        "0-0"
+
+[<Fact>]
+let ``0-0-0 movement name`` () =
+    { Piece = King
+      FromCoords = (7, 4)
+      ToCoords = (7, 2)
+      IsCapture = false
+      Promotion = None
+      IsCheck = false
+      IsMate = false
+      IsStalemate = false
+      SamePieceCoords = None }    
+    |> movementName false true
+    |> should equal
+        "0-0-0"
+
+[<Fact>]
+let ``bxc3 movement name`` () =
+    { Piece = Pawn
+      FromCoords = (1, 3)
+      ToCoords = (2, 2)
+      IsCapture = true
+      Promotion = None
+      IsCheck = false
+      IsMate = false
+      IsStalemate = false
+      SamePieceCoords = Some (1, 1) }    
+    |> movementName true false
+    |> should equal
+        "dxc3"
+
+[<Fact>]
+let ``gxh1=♜# movement name`` () =
+    { Piece = Pawn
+      FromCoords = (1, 6)
+      ToCoords = (0, 7)
+      IsCapture = true
+      Promotion = Some Rook
+      IsCheck = true
+      IsMate = true
+      IsStalemate = false
+      SamePieceCoords = None }    
+    |> movementName false true
+    |> should equal
+        "gxh1=♜#"
+
+[<Fact>]
+let ``N1b3+ movement name`` () =
+    { Piece = Knight
+      FromCoords = (0, 0)
+      ToCoords = (2, 1)
+      IsCapture = false
+      Promotion = None
+      IsCheck = true
+      IsMate = false
+      IsStalemate = false
+      SamePieceCoords = Some (4, 0) }    
+    |> movementName false false
+    |> should equal
+        "N1b3+"
+
+[<Fact>]
+let ``♕axh2 movement name`` () =
+    { Piece = Queen
+      FromCoords = (1, 0)
+      ToCoords = (1, 7)
+      IsCapture = true
+      Promotion = None
+      IsCheck = false
+      IsMate = false
+      IsStalemate = false
+      SamePieceCoords = Some (7, 7) }    
+    |> movementName true true
+    |> should equal
+        "♕axh2"
+
