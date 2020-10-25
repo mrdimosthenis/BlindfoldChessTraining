e671d1c042b2b9dbe55adfb25468ba6b30b6354b
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-10-25 13:57:40 +0200

    scroll options

--- BlindfoldChessTraining/Page.Options.fs
+++ BlindfoldChessTraining/Page.Options.fs
@@ -5,7 +5,11 @@ open Fabulous
 open Fabulous.XamarinForms
 open Fabulous.XamarinForms.LiveUpdate
 open Xamarin.Forms
+
 open BlindfoldChessTraining
+open BlindfoldChessTraining.UIElems
+
+open BlindfoldChessMechanics.Logic
 
 let view (model: Model.Model) (dispatch: Msg.Msg -> unit): ViewElement =
     let accentTitle = "Speech Accent:"
@@ -34,40 +38,49 @@ let view (model: Model.Model) (dispatch: Msg.Msg -> unit): ViewElement =
             verticalOptions = LayoutOptions.Center,
             children = [
                 View.Label(text = "Options", fontAttributes = FontAttributes.Bold, horizontalOptions = LayoutOptions.Center)
-                View.StackLayout(
-                    orientation = StackOrientation.Horizontal,
-                    horizontalOptions = LayoutOptions.Center,
-                    children = [
-                        View.Label(text = "Display Board Coordinates:")
-                        View.CheckBox(
-                            isChecked = model.ConfigOptions.AreCoordsEnabled,
-                            checkedChanged = (fun args -> model.ConfigOptions.AreCoordsEnabled |> not |> Msg.SelectCoordsConfig |> dispatch)
-                        )
-                    ]
-                )
+                View.ScrollView(
+                    content = View.StackLayout(
+                        horizontalOptions = LayoutOptions.Center,
+                        verticalOptions = LayoutOptions.Center,
+                        children = [
+                            Board.grid model.ConfigOptions.AreCoordsEnabled Board.init
+                            View.StackLayout(
+                                orientation = StackOrientation.Horizontal,
+                                horizontalOptions = LayoutOptions.Center,
+                                children = [
+                                    View.Label(text = "Display Board Coordinates:")
+                                    View.CheckBox(
+                                        isChecked = model.ConfigOptions.AreCoordsEnabled,
+                                        checkedChanged = (fun args -> model.ConfigOptions.AreCoordsEnabled |> not |> Msg.SelectCoordsConfig |> dispatch)
+                                    )
+                                ]
+                            )
 
-                View.Label(
-                    text = sprintf "Speech Pitch: %.1f" model.ConfigOptions.SpeechPitch,
-                    horizontalTextAlignment = TextAlignment.Center
-                )
-                View.Slider(
-                    minimumMaximum = (0.1, 2.0),
-                    valueChanged = (fun args -> args.NewValue |> Msg.SelectPitchConfig |> dispatch),
-                    value = model.ConfigOptions.SpeechPitch
-                )
+                            View.Label(
+                                text = sprintf "Speech Pitch: %.1f" model.ConfigOptions.SpeechPitch,
+                                horizontalTextAlignment = TextAlignment.Center
+                            )
+                            View.Slider(
+                                minimumMaximum = (0.1, 2.0),
+                                valueChanged = (fun args -> args.NewValue |> Msg.SelectPitchConfig |> dispatch),
+                                value = model.ConfigOptions.SpeechPitch
+                            )
 
-                View.Label(
-                    text = accentTitle,
-                    horizontalTextAlignment = TextAlignment.Center
+                            View.Label(
+                                text = accentTitle,
+                                horizontalTextAlignment = TextAlignment.Center
+                            )
+                            accentPicker
+                            View.Button(text = "Speak", horizontalOptions = LayoutOptions.Center, command = fun () -> Speech.speak model "blindfold chess training")
+                            View.Label(
+                                text = "Please make sure that the accent is actually supported by the device. By pressing the button above, you should hear an example of the accent.",
+                                horizontalTextAlignment = TextAlignment.Center
+                            )
+                            
+                            View.Button(text = "Back", horizontalOptions = LayoutOptions.Center, command = fun () -> dispatch (Msg.SelectPage Model.HomePage))
+                        ]
+                    )
                 )
-                accentPicker
-                View.Button(text = "Speak", horizontalOptions = LayoutOptions.Center, command = fun () -> Speech.speak model "blindfold chess training")
-                View.Label(
-                    text = "Please make sure that the accent is actually supported by the device. By pressing the button above, you should hear an example of the accent.",
-                    horizontalTextAlignment = TextAlignment.Center
-                )
-                
-                View.Button(text = "Back", horizontalOptions = LayoutOptions.Center, command = fun () -> dispatch (Msg.SelectPage Model.HomePage))
             ]
         )
     )
