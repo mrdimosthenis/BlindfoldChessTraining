21b80905398a006362d104ae6b1a6f481cfbb142
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-11-15 21:13:26 +0200

    delete appLogo

--- BlindfoldChessTraining/UIElems.Images.fs
+++ BlindfoldChessTraining/UIElems.Images.fs
@@ -3,8 +3,6 @@
 open Fabulous.XamarinForms
 open BlindfoldChessTraining
 
-let appLogo : Image.Value = Resources.image "logos.app_logo"
-
 let main : Image.Value = Resources.image "logos.main"
 
 let fabulous : Image.Value = Resources.image "logos.fabulous"
