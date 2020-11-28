2775ac96db46a4c42c9e4c6845ae4681dc1f7886
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-11-28 19:16:02 +0200

    create index evaluated_line_is_parsed

--- BlindfoldChessEngine/InitDB.sql
+++ BlindfoldChessEngine/InitDB.sql
@@ -6,6 +6,7 @@
 	is_parsed bool not null default false
 );
 create unique index evaluated_line_i_month on evaluated_line (i, "month");
+create index evaluated_line_is_parsed on evaluated_line (is_parsed);
 
 CREATE TABLE position (
 	evaluated_line_id int not null,
