88e3108251be5c4735b561f14c3b2231ed755293
mrdimosthenis <mrdimosthenis@hotmail.com>
2021-02-11 15:30:32 +0200

    "starts from the top"

--- BlindfoldChessTraining/Page.Description.fs
+++ BlindfoldChessTraining/Page.Description.fs
@@ -8,9 +8,9 @@ open BlindfoldChessTraining.Template
 
 let audioTrainingDescription: string =
     if Constants.isIOSDevice then
-        "Swipe left and right to listen to the puzzle description. Swipe right describes the puzzle sentence by sentence. Swipe left starts over the top."
+        "Swipe left and right to listen to the puzzle description. Swipe right describes the puzzle sentence by sentence. Swipe left starts from the top."
     else
-        "Press the volume keys on your device to listen to the puzzle description. Volume down key describes the puzzle sentence by sentence, while volume up key starts over the top."
+        "Press the volume keys on your device to listen to the puzzle description. Volume down key describes the puzzle sentence by sentence, while volume up key starts from the top."
 
 let description: string =
     sprintf """Improve your blindfold thinking by solving chess puzzles without diagrams. Read or listen to each puzzle description and try to find the best move. Train yourself while walking, jogging or in transportation.
