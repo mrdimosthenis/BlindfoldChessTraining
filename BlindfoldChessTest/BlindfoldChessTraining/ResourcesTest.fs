848f1de21818f1df4955fdfb8790ff97b9536c40
mrdimosthenis <mrdimosthenis@hotmail.com>
2023-04-09 17:51:04 +0300

    Complete tests

---
+++ BlindfoldChessTest/BlindfoldChessTraining/ResourcesTest.fs
@@ -0,0 +1,40 @@
+﻿module BlindfoldChessTest.BlindfoldChessTraining.ResourcesTest
+
+open BlindfoldChessMechanics
+open BlindfoldChessTraining
+open FSharpx.Collections
+open NUnit.Framework
+
+let endgamePuzzles =
+    "BlindfoldChessTraining.resources.puzzles.endgame_puzzles.jsonl"
+    |> Resources.lines
+    |> LazyList.filter (fun (s: string) -> s.Trim() <> "")
+    |> LazyList.toArray
+
+let openingPuzzles =
+    "BlindfoldChessTraining.resources.puzzles.opening_puzzles.jsonl"
+    |> Resources.lines
+    |> LazyList.filter (fun (s: string) -> s.Trim() <> "")
+    |> LazyList.toArray
+
+[<Test>]
+let ``parse and emit endgame puzzles`` () =
+    let act =
+        endgamePuzzles
+        |> LazyList.ofArray
+        |> LazyList.map Notation.Parser.jsonOfGame
+        |> LazyList.map Notation.Emitter.gameJson
+        |> LazyList.toArray
+
+    Assert.AreEqual(endgamePuzzles, act)
+
+[<Test>]
+let ``parse and emit opening puzzles`` () =
+    let act =
+        openingPuzzles
+        |> LazyList.ofArray
+        |> LazyList.map Notation.Parser.jsonOfGame
+        |> LazyList.map Notation.Emitter.gameJson
+        |> LazyList.toArray
+
+    Assert.AreEqual(openingPuzzles, act)
---
+++ BlindfoldChessTraining/Resources.fs
@@ -0,0 +1,15 @@
+﻿module BlindfoldChessTraining.Resources
+
+open FSharpx.Collections
+open System.IO
+open System.Reflection
+
+let lines resourceName =
+    seq {
+        use st = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)
+        use sr = new StreamReader(st)
+
+        while not sr.EndOfStream do
+            yield sr.ReadLine()
+    }
+    |> LazyList.ofSeq
